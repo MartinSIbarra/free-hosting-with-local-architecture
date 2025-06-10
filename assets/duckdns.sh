7bc82144bd5ecc5e7fe9384c236471303539d9ff
Martín Ibarra <martinibarra@gmail.com>
2025-06-10 10:24:10 -0300

    fix done on duckdns service logs

--- assets/duckdns.sh
+++ assets/duckdns.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # This script updates the DDNS record for a domain using DuckDNS.
 mkdir -p $HOME/logs
-echo curl -s "https://www.duckdns.org/update?domains=app-owfema&token=$DUCKDNS_TOKEN&ip=" >> $HOME/logs/duckdns.log
-echo " - DuckDNS updated at $(date)" >> $HOME/logs/duckdns.log
+command='curl -s "https://www.duckdns.org/update?domains=app-owfema&token=$DUCKDNS_TOKEN&ip="'
+echo "$(date) Updating DuckDNS..." >> $HOME/logs/duckdns.log
+echo "$command" >> $HOME/logs/duckdns.log
+eval $command >> $HOME/logs/duckdns.log
