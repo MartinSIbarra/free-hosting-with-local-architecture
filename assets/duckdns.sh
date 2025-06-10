0b78da015acb37c553915ad3273842fe5c45e425
Martín Ibarra <martinibarra@gmail.com>
2025-06-10 10:47:46 -0300

    some fix done to correcto envsubst on duckdns.sh

--- assets/duckdns.sh
+++ assets/duckdns.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # This script updates the DDNS record for a domain using DuckDNS.
 mkdir -p $HOME/logs
-command='curl -s "https://www.duckdns.org/update?domains=app-owfema&token=$DUCKDNS_TOKEN&ip="'
 echo "$(date) Updating DuckDNS..." >> $HOME/logs/duckdns.log
-echo "$command" >> $HOME/logs/duckdns.log
-eval $command >> $HOME/logs/duckdns.log
+echo 'curl -s "https://www.duckdns.org/update?domains=app-owfema&token=$DUCKDNS_TOKEN&ip="' >> $HOME/logs/duckdns.log
+curl -s "https://www.duckdns.org/update?domains=app-owfema&token=$DUCKDNS_TOKEN&ip=" >> $HOME/logs/duckdns.log
