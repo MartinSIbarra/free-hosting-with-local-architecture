03c92f5bbfcf2b30ff4031599baf81c5be8b302b
Martín Ibarra <martinibarra@gmail.com>
2025-06-10 10:58:26 -0300

    minor fix

--- assets/duckdns.sh
+++ assets/duckdns.sh
@@ -4,3 +4,4 @@ mkdir -p $HOME/logs
 echo "$(date) Updating DuckDNS..." >> $HOME/logs/duckdns.log
 echo 'curl -s "https://www.duckdns.org/update?domains=app-owfema&token=$DUCKDNS_TOKEN&ip="' >> $HOME/logs/duckdns.log
 curl -s "https://www.duckdns.org/update?domains=app-owfema&token=$DUCKDNS_TOKEN&ip=" >> $HOME/logs/duckdns.log
+echo -e "\n"
